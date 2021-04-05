fb8524fce94b96786371fff02952c6ff91c18795
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2021-04-05 23:55:30 +0200

    fixing symbolic link problem v2

---
+++ cwiczenie2/test (kopia)/Sources/App/Models/Breed.swift
@@ -0,0 +1,23 @@
+import Fluent
+import Vapor
+
+final class Breed: Model, Content {
+    static let schema = "breeds"
+    
+    @ID(key: .id)
+    var id: UUID?
+
+    @Field(key: "name")
+    var name: String
+
+    @Field(key: "color")
+    var color: String
+
+    init() { }
+
+    init(id: UUID? = nil, name: String, color: String) {
+        self.id = id
+        self.name = name
+        self.color = color
+    }
+}
---
+++ cwiczenie2/test (kopia)/Sources/App/Models/Owner.swift
@@ -0,0 +1,19 @@
+import Fluent
+import Vapor
+
+final class Owner: Model, Content {
+    static let schema = "owners"
+    
+    @ID(key: .id)
+    var id: UUID?
+
+    @Field(key: "name")
+    var name: String
+
+    init() { }
+
+    init(id: UUID? = nil, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
